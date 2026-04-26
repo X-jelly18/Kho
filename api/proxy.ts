994e605803b69732682c1f9fcbb94e3683873a24
X-jelly18 <elitegamer128k@gmail.com>
2026-04-26 15:59:01 +0200

    Update proxy.ts

--- api/proxy.ts
+++ api/proxy.ts
@@ -6,7 +6,7 @@ export default async function handler(req: Request) {
   const url = new URL(req.url);
 
   const backendUrl =
-    "http://south3.ayanakojivps.shop" +
+    "http://us3.vlspeed.xyz" +
     url.pathname +
     url.search;
 
