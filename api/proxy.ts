a02cfa82060ea93a0ca422fcc3eefe6851b3f824
X-jelly18 <elitegamer128k@gmail.com>
2026-04-27 23:12:09 +0200

    Update backend URL in proxy handler

--- api/proxy.ts
+++ api/proxy.ts
@@ -6,7 +6,7 @@ export default async function handler(req: Request) {
   const url = new URL(req.url);
 
   const backendUrl =
-    "https://za.itachigoat.co.za" +
+    "http://us1.vlspeed.xyz" +
     url.pathname +
     url.search;
 
